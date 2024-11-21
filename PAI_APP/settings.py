9e2072b6f4f6bfe35238b41be8075dfbaf4e4d5e
akonomy <andrei.spac@student.usv.ro>
2024-11-21 11:27:57 +0200

    license

--- PAI_APP/settings.py
+++ PAI_APP/settings.py
@@ -29,7 +29,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 # Application definition
-
+django_heroku.settings(locals())
 
 
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PAI_APP.wsgi
