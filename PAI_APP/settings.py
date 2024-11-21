5f3e6cc72156769e91ad4ed628a50ee0efab7231
akonomy <andrei.spac@student.usv.ro>
2024-11-21 02:12:11 +0200

    f

--- PAI_APP/settings.py
+++ PAI_APP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=5kpns*l_l@*(q6-7c%8!ue&0u4g4+^sdsi_h60lm2va(co@l=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.10.164','127.0.0.1','myapp','10.11.1.122', '10.11.1.125']
+ALLOWED_HOSTS = ['django-plan-pai-e5f2gvhthcapgag5.canadacentral-01.azurewebsites.net/','192.168.10.164','127.0.0.1','myapp','10.11.1.122', '10.11.1.125']
 
 
 # Application definition
